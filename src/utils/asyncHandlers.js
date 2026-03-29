cdec9a380bffe138f77268e8403ce626b50b3ed3
Saif Ali Khan <saifalikhan123@example.com>
2026-03-29 13:28:41 -0700

    utils code discussion

--- src/utils/asyncHandlers.js
+++ src/utils/asyncHandlers.js
@@ -1,4 +1,9 @@
-const asyncHandler=()=>{}
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+      Promise.resolve(requestHandler(req,res,next)).
+      catch((error)=> next(error))
+    }
+}
 
 
 
