a414676003917b12d1c80cb3f9730f6ba4dc9311
Saif Ali Khan <saifalikhan123@example.com>
2026-04-04 13:08:05 -0700

    multer middleware

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -19,7 +19,7 @@ const userSchema=new Schema(
         lowercase:true,
         trim:true,
       },
-      fullname:{
+      fullName:{
         type:String,
         required:true,
         trim:true,
@@ -57,6 +57,39 @@ const userSchema=new Schema(
     this.password= bcrypt.hash(this.password, 10)
     next()
  })
+
+   userSchema.methods.isPasswordCorrect= async function (password){
+   return await bcrypt.compare(password,this.password)
+   }
+
+ userSchema.methods.generateAccessToken= function (){
+    
+    return jwt.sign(
+    {
+      _id:this.id,
+      email:this.email,
+      username:this.username,
+      fullName:this.fullName
+    },
+     process.env.ACCESS_TOKEN_SECRET,
+     {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+     }
+   )
+ }
+
+ userSchema.methods.generateRefreshToken= function (){
+  return jwt.sign(
+    {
+      _id:this.id,
+      
+    },
+     process.env.REFRESH_TOKEN_SECRET,
+     {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+     }
+   )
+ }
 
 
 export const User= mongoose.model("User",userSchema);
